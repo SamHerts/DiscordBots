287e598a1b9f0b91a4571e22b6b5175265843e04
Samuel Herts <sdherts@gmail.com>
2021-06-28 11:50:18 -0500

    Updated Formatting and added Tweepy Streaming

--- TweetyBird/main.py
+++ TweetyBird/main.py
@@ -5,54 +5,23 @@ import os
 from discord.ext import commands
 import tweepy
 
-#client = discord.Client()
+
+
+#Secret Keys
 Discord_Token = os.environ['Discord_Token']
 Twitter_API_PK = os.environ['Twitter_API_PK']
 Twitter_API_SK = os.environ['Twitter_API_SK']
 Twitter_Access_Token = os.environ['Twitter_Access_Token']
 Twitter_Access_Secret = os.environ['Twitter_Access_Secret']
+
+#Descriptions
 TweetyBirdDesc = "TweetyBird Discord Bot - By SamH."
 FollowDescription = "Add a twitter account to the subscription list"
 UnFollowDescription = "Remove a twitter account from the subscription list"
 ListUsersDescription = "See what twitter accounts are being followed"
+########FUNCTION DEFINITIONS#############
 
-bot = commands.Bot(command_prefix='!', case_insensitive=True, description=TweetyBirdDesc)
-auth = tweepy.OAuthHandler(Twitter_API_PK,Twitter_API_SK)
-auth.set_access_token(Twitter_Access_Token,Twitter_Access_Secret)
-api = tweepy.API(auth)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-
-@bot.command(description=FollowDescription)
-async def Follow(ctx, user: str):
-    print('Got Follow Command')
-    print(user)
-    await ctx.send(update_Following(user))
-
-@bot.command(description=UnFollowDescription)
-async def UnFollow(ctx, user: str):
-    print('Got UnFollow Command')
-    print(user)
-    await ctx.send(delete_Following(user))
-
-@bot.command(description=ListUsersDescription)
-async def ListUsers(ctx):
-    print('Listing Subscription List')
-    await ctx.send(get_Following())
-
-
+#Add a username to the follow list
 def update_Following(TwitterUser):
     if "TwitterFollows" in db.keys():
         TwitterFollows = db["TwitterFollows"]
@@ -67,6 +36,7 @@ def update_Following(TwitterUser):
         return 'Adding ' + TwitterUser + ' to Follow List'
 
 
+#Remove a username from the follow list
 def delete_Following(TwitterUser):
     TwitterFollows = db["TwitterFollows"]
     if TwitterUser in TwitterFollows:
@@ -77,6 +47,7 @@ def delete_Following(TwitterUser):
         return "User was not found in the subscription list."
 
 
+#Print a list of followed users
 def get_Following():
     emptyFollows = "You do not currently follow any Twitter Users. Use '!Follow $AccountName' to subscribe to their tweets."
 
@@ -88,6 +59,82 @@ def get_Following():
             return " ,".join(TwitterFollows)
     else:
         return emptyFollows
+
+
+def Get_Follow_IDs(api):
+    #Take in list of usernames -- return list of user IDs
+    username_list = []
+    if "TwitterFollows" in db.keys():
+        TwitterFollows = db["TwitterFollows"]
+        if len(TwitterFollows) == 0:
+            return ""
+        else:
+            for username in TwitterFollows:
+                username_list.append(api.get_user(username).id_str)
+    else:
+        return ""
+
+
+########END FUNCTION DEFINITIONS#############
+
+#Discord Bot Setup
+bot = commands.Bot(command_prefix='!',
+                   case_insensitive=True,
+                   description=TweetyBirdDesc)
+
+# Authenticate with Twitter
+auth = tweepy.OAuthHandler(Twitter_API_PK, Twitter_API_SK)
+auth.set_access_token(Twitter_Access_Token, Twitter_Access_Secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+
+#Tweepy Streaming tweets
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+myStream.filter(follow=Get_Follow_IDs(), is_async=True)
+
+
+#Discord Bot Ready and Commands
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command(description=FollowDescription)
+async def Follow(ctx, user: str):
+    print('Got Follow Command')
+    print(user)
+    await ctx.send(update_Following(user))
+
+
+@bot.command(description=UnFollowDescription)
+async def UnFollow(ctx, user: str):
+    print('Got UnFollow Command')
+    print(user)
+    await ctx.send(delete_Following(user))
+
+
+@bot.command(description=ListUsersDescription)
+async def ListUsers(ctx):
+    print('Listing Subscription List')
+    await ctx.send(get_Following())
+
+
+
+
 
 #keep_alive()
 bot.run(Discord_Token)
