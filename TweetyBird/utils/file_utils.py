88748576a5518dbcbeafb188f679178c095d50c4
topher235 <topherhurt@gmail.com>
2021-06-30 23:29:26 -0400

    Adds some semblance of data persistence

---
+++ TweetyBird/utils/file_utils.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+
+def open_json_file(filepath):
+    if not os.path.exists(filepath):
+        os.write(filepath, str.encode('{}'))
+
+    with open(filepath, 'r') as file:
+        contents = {}
+        try:
+            contents = json.load(file)
+        except json.JSONDecodeError:  # This can happen if the file contents are empty
+            pass
+        return contents
+
+
+def save_json_file(filepath, contents):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+            with open(filepath, 'w') as file:
+                file.write(json.dumps(contents))
+        return wrapper
+    return decorator
