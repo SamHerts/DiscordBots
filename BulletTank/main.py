dd0892e7d5427357d36776627f7339c6c2d4dc3c
Samuel Herts <sdherts@gmail.com>
2021-08-02 11:22:49 -0500

    Create main.py

---
+++ BulletTank/main.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+
+from utils import settings
+
+# Discord Bot Setup
+BulletTankDescription = "Bullet Tank Discord Bot - By SamH."
+
+bot = commands.Bot(command_prefix='?',
+                   case_insensitive=True,
+                   description=TweetyBirdDescription)
+
+
+@bot.event
+async def on_ready():
+    """
+    Triggers when bot starts.
+    """
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(error)
+    await ctx.send(error)
+
+if __name__ == "__main__":
+    bot.load_extension("Cogs.MainCog")    
+    bot.run(settings.Discord_Token)
