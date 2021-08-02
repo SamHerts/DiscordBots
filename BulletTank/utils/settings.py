11df38f0da95dbce9f360b6656bd4cd25a210bbb
Samuel Herts <sdherts@gmail.com>
2021-08-02 11:40:07 -0500

    update settings

--- BulletTank/utils/settings.py
+++ BulletTank/utils/settings.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
-Discord_Token = os.getenv('Discord_Token', None)
+Discord_Token = os.getenv('BT_Discord_Token', None)
 Discord_Webhook = os.getenv('Discord_WebHook', None)
