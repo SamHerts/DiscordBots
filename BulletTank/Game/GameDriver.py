65d68de799ad7d6f0c0f2f78cc8d0ad9194aca8b
Samuel Herts <sdherts@gmail.com>
2021-08-25 16:07:31 -0500

    removed unneeded comment

--- BulletTank/Game/GameDriver.py
+++ BulletTank/Game/GameDriver.py
@@ -11,22 +11,6 @@ except ImportError:
     except ImportError:
         raise
 
-"""
-Rules:
-
-Last Player standing wins.
-Each day, players receive at least 1 Action Point.
-Action points are lost upon use
-Action points can be transferred to other players
-Actions you can take:
-Move one space
-Shoot at a player within range
-Increase range - up to 3
-Transfer an Action Point to another Player
-Each Player starts with 4 hearts, if you run out of hearts you die.
-Defeated Players are added to the Angel Box and vote to give out bonus Action Points.
-A player must receive 30% of the vote to receive the Action Point
-"""
 
 blank_grid = None
 grid_size = [20, 10]
